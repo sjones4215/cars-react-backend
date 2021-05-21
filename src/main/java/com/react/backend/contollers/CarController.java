7776e2a784eb126fc96a2c6f8d4027df5e2bc3e2
sjones4215 <bluesfansince2013@gmail.com>
2021-05-21 17:00:16 -0500

    sending data to the frontend

--- src/main/java/com/react/backend/contollers/CarController.java
+++ src/main/java/com/react/backend/contollers/CarController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/v1/cars")
 public class CarController {
--- src/main/java/com/react/backend/models/Car.java
+++ src/main/java/com/react/backend/models/Car.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.util.List;
 
+
 @Entity(name = "cars")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Car {
